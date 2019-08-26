895468e7fee19b6dc014d260b468f72e18d6a0ea
Odunayo Okebunmi <okebunmiodunayo@gmail.com>
2019-08-27 00:42:16 +0100

    setup(test): set up test environment adds jest and enzyme dependecies test App component [Starts #168095389]

---
+++ src/components/App.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+
+describe('App', () => {
+  it('should render without crashing', () => {
+    shallow(<App />);
+  });
+});
