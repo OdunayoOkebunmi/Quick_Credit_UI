1f01d09206f74b2531642f2d6ad5ed943948fdcb
Odunayo Okebunmi <okebunmiodunayo@gmail.com>
2019-08-27 15:34:19 +0100

    chore(setup):update README [Starts #168139742]

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,20 @@
+const { defaults } = require('jest-config');
+
 module.exports = {
+  verbose: true,
+  clearMocks: true,
+  collectCoverage: false,
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+    },
+  },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|svg|cass|scss|less|css)$': '<rootDir>/__mocks__/fileMock.js',
   },
   setupFilesAfterEnv: ['<rootDir>src/setupTests.js'],
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
 };
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,6 @@
-/* eslint-disable no-undef */
 import React from 'react';
 import { shallow } from 'enzyme';
 import Home from './Home';
-
 
 describe('App', () => {
   it('should render without crashing', () => {
